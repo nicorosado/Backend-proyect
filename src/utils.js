c9fa0d30eec7474f93458f559cae471c13fc558c
nicorosado <nicorosado.nr@gmail.com>
2023-08-09 22:41:30 -0300

    minor changes

--- src/utils.js
+++ src/utils.js
@@ -20,7 +20,7 @@ import config from "./config/config.js";
 
 // CONECTAR MONGO
 export async function connectMongo() {
-  
+
   try {
     await connect(
       config.mongoUrl
@@ -32,6 +32,8 @@ export async function connectMongo() {
   }
 }
 //hash
+
+
 import bcrypt from 'bcrypt';
 export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 export const isValidPassword = (password, hashPassword) => bcrypt.compareSync(password, hashPassword);
