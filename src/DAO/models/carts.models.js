7df3a5a6a2f18064fde8c1db70d6bbcfc321d207
nicorosado <nicorosado.nr@gmail.com>
2023-08-16 21:19:08 -0300

    minor changes

--- src/DAO/models/carts.models.js
+++ src/DAO/models/carts.models.js
@@ -1,18 +1,19 @@
 import { Schema, model } from "mongoose";
 
+
 const schema = new Schema({
-  products:{
-    type:[{
-        product:{
-             type: Schema.Types.ObjectId,
-             ref:'products',
-             index:true,
-        },
-        quantity:{type:Number},
-  
+  products: {
+    type: [{
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: 'products',
+        index: true,
+      },
+      quantity: { type: Number },
+
     }],
     default: [],
-  } 
+  }
 });
 
 export const CartsModel = model("carts", schema);
